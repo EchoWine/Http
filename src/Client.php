b23c0602b3860fd6b252764c2cf9b3c6bc03623f
EchoWine <dimitri.who@gmail.com>
2016-10-09 17:15:16 +0200

    Fix GET request Client

--- src/Client.php
+++ src/Client.php
@@ -36,21 +36,16 @@ class Client{
 		if($method == 'GET' && !empty($params))
 			$url .= "?".http_build_query($params);
 
-		if($method == 'GET')
-			return file_get_contents($url);
-
 		curl_setopt($ch, CURLOPT_URL,$url);
-		curl_setopt($ch, CURLOPT_POST, 1);
 
-		if($method == 'POST')
+		if($method == 'POST'){
+			curl_setopt($ch, CURLOPT_POST, 1);
 			curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($params));
+		}
 
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 		$response = curl_exec($ch);
-
-		print_r($url);
-		print_r($method);
 
 		curl_close($ch);
 
