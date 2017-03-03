fea7be509e11fdd10f39b51959f7f3194693acc2
EchoWine <dimitri.who@gmail.com>
2017-03-03 22:27:07 +0100

    Added timeout

--- src/Client.php
+++ src/Client.php
@@ -50,6 +50,8 @@ class Client{
 		
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); 
+		curl_setopt($ch, CURLOPT_TIMEOUT, 10*60);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT , 30); 
 
 
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
